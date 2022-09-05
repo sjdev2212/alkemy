6184bc47725ba1c4e2edb26972a728511c12fd96
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-09-05 09:57:29 -0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login'
+import Listado from './components/Listado'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Detalle from './components/Detalle'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route path="listado" element={<Listado />} />
+      <Route path="detalle/:id" element={<Detalle />} />
+     
+    </Routes>
+    <Footer/>
+      </BrowserRouter>
+
   );
 }
 
