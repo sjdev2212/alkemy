827fa6919164bf8f02a121d86128b98897d95a3c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-09-06 15:34:34 -0300

    get info from api

--- src/components/Resultados.js
+++ src/components/Resultados.js
@@ -1,14 +1,26 @@
-import React from 'react'
 import {useState,useEffect} from 'react'
+import axios from "axios";
+
 import {useParams} from 'react-router-dom'
 
 const Resultados = () => {
     const keyWord = useParams()
-  const apiCall = `https://api.themoviedb.org/3/search/movie?api_key=5e63572e6af98e257733a794be08a3f8&language=en-US&query`
+    console.log(keyWord.id)
+  const apiCall = `https://api.themoviedb.org/3/search/movie?api_key=5e63572e6af98e257733a794be08a3f8&language=en-US&query=${keyWord.id}`
     const [resultados, setResultados] = useState([])
 
     useEffect(() => {
-        
+axios.get(apiCall).then(response => {
+    const results =  response.data
+    console.log(results.results)
+ 
+  
+})
+.catch(err => console.log(err) ) 
+
+}, [])
+ 
+
   return (
     <div>Resultados
 {keyWord.id}
