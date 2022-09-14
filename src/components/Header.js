1ac3f55c74f637fbf323ed160880a5d355ecd023
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-09-14 10:39:59 -0300

    modify header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,24 @@
 import React from 'react'
 import{Link} from 'react-router-dom'
-import Buscardor from './Buscador'
+import Buscador from './Buscador'
+import './styles/header.css'
+
 const Header = () => {
   return (
-<nav className="navbar navbar-expand-lg bg-success">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to='/' >Navbar</Link>
-   
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
- 
-        <li className="nav-item">
-          <Link className="nav-link" to='/listado'>Listado</Link>
-        </li>
-       
-        <li className="nav-item">
-          <Link className="nav-link" to='/favoritos'>Favoritos</Link>
-        </li>
-     
-      </ul>
-    </div>
-  </div>
-  <Buscardor/>
-</nav>
+  <nav className="header">
+     <ul  className="header-ul">
+       <li>
+         <Link  className="header-li" to='/'>Home</Link>
+       </li>
+       <li>
+         <Link  className="header-li" to='/listado'>Movie List</Link>
+       </li>
+       <li>
+         <Link  className="header-li"  to='/favoritos'>Favorites</Link>
+       </li>
+     </ul>
+   <Buscador/>
+  </nav>
 
   )
 }
